0bb9594640f3f833b2fef8dace937603269d06d2
somi-thoughts <somithoughts@gmail.com>
2025-08-03 19:11:49 +0000

    initial

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using Markdig;
+
+namespace somi_thoughts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {            
+            string layout = File.ReadAllText("layout.html");
+            string inputDir = "thoughts";
+            string outputDir = "output";
+
+            if(Directory.Exists(outputDir))
+                Directory.Delete(outputDir, true);
+            Directory.CreateDirectory(outputDir);
+            Directory.CreateDirectory(Path.Combine(outputDir, "thoughts"));
+
+            foreach (var pagePath in Directory.GetFiles(inputDir))
+            {
+                var md = File.ReadAllText(pagePath);
+                string finalHtml = SetContentMarkdown(layout, md);
+                var fileName = Path.GetFileNameWithoutExtension(pagePath) + ".html";
+                var outputPath = Path.Combine(outputDir,"thoughts", fileName);
+                File.WriteAllText(outputPath, finalHtml);
+            }
+
+            string thoughsHtml = "";
+            thoughsHtml += "<ul class=link-holder>";
+            for (int i = 0; i < 1; i++)
+            {
+                foreach (var pagePath in Directory.GetFiles(inputDir).OrderDescending())
+                {
+                    string thoughtId = Path.GetFileNameWithoutExtension(pagePath);
+                    thoughsHtml += $"<li><a href=\"thoughts/{thoughtId + ".html"}\" class=\"link\">{thoughtId.Replace("-", " – ")}</a></li>";
+                }
+            }
+
+            var index = SetTag(layout, "content", thoughsHtml);
+            thoughsHtml += "</ul>";
+            File.WriteAllText(Path.Combine(outputDir, "index.html"), index);
+            
+            
+            var about = SetTag(layout, "content", 
+$@"
+<p>
+This website holds some raw unfiltered thoughts that I want to write down anonymously. They are written from me to <span class=""intro"">S o m i</span>. See them as writings from from a guy with a mild psychosis written to his crush that only exist in his mind.
+</p>
+
+<h2>
+Anonymous
+</h2>
+<p>
+Some of these thoughts are risky so just in case someone tries, the domain is bought from an anonomoys domain registry using crypto I got from a shady crypto exchange. The email account is created using a new prepaid sim card. Everything from account creation and commits are done on a VM running Whonix. First time I actually needed privacy and it was quite a hastle especially how each site blocks Tor activity. Anyways I tried my best to stay hidden, I hope it stays that way.
+</p>
+<br/>
+<h2>
+Contact
+</h2>
+<p>
+If you want to contact me you can email me at somi thoughts @ gmail . com (remove spaces) or via GitHub. Be aware that I only sporadiclly log in. 
+</p>
+");
+            File.WriteAllText(Path.Combine(outputDir, "about.html"), about);
+        }
+
+        private static string SetContentMarkdown(string layout, string content)
+        {
+            var indexTag = layout.IndexOf("<!--[content]-->");
+            string finalHtml = SetTag(layout, "content", " <div class=\"markdown\">" + Markdown.ToHtml(content) + " </div>");
+            int end = finalHtml.IndexOf("Somi",indexTag, StringComparison.Ordinal) + "Somi".Length;
+            int start = finalHtml[..end].LastIndexOf(">", StringComparison.Ordinal) + 1;
+            var intro = finalHtml[start..end];
+            finalHtml = finalHtml.Replace(intro, $"<span class=\"intro\">{intro}</span>");
+            return finalHtml;
+        }
+
+        private static string SetTag(string layout, string tag, string content)
+        {
+            var finalHtml = layout.Replace($"<!--[{tag}]-->", content);
+            return finalHtml;
+        }
+    }
+}
---
+++ thoughts/000-Relative life enjoyment.md
@@ -0,0 +1,4 @@
+Fk this Somi. I have to have a form of autism. There is something wrong, I know it for sure now. Is this why I like programming so much? Is it that I can't get the positive emotions from social contact because of my limitations which causes me to get less total joy out of life? so relatively boring stuff like programming behind a screen all day is fun for me? If I where capable to experience social interactions like normies would I want to party and date and stuff? That is why I can't explain it to normies, because they already understand it, they just experience different levels of joy that I can never experience. 
+
+Please I want to get of this train and end this trip. Get me out.
+
